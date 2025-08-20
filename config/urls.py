0359a35403cac1b0c425549780182904d21672d2
hoochucat <ohnanabanha@gmail.com>
2025-08-21 00:54:14 +0900

    ✨ 3일차 과제 완료! :)
    수정, 삭제기능 구현

--- config/urls.py
+++ config/urls.py
@@ -19,14 +19,17 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 
-from todo.views import todo_list, todo_info
+from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete
 
 from users import views as user_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('todo/', todo_list, name='todo_list'),
-    path('todo.<int:todo_id>/', todo_info, name='todo_info'),
+    path('todo/create/', todo_create, name= 'todo_create'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path('todo/<int:todo_id>/update/', todo_update, name= 'todo_update'),
+    path('todo/<int:todo_id>/delete/', todo_delete, name= 'todo_delete'),
+    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', user_views.login, name= 'login'),
     path('accounts/signup/', user_views.sign_up, name= 'signup'),
