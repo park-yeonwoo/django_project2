b7d8d67a9b2bd4f91ead5e41a0051c0a637b3a19
hoochucat <ohnanabanha@gmail.com>
2025-08-22 17:03:32 +0900

    ✨ 6일차 과제 완료
    :)

--- config/settings.py
+++ config/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIRS = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    STATIC_DIRS,
+]
+STATIC_ROOT = BASE_DIR / '.static_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- todo/admin.py
+++ todo/admin.py
@@ -1,19 +1,41 @@
-from django.contrib import admin
-from todo.models import Todo
+# todo > admin.py
 
-# Register your models here.
+from django.contrib import admin
+from todo.models import Todo, Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+    fields = ('message', 'user')
+
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'is_completed', 'start_date','end_date')
+    list_display = ('id', 'user', 'title', 'description', 'is_completed', 'start_date', 'end_date')
     list_filter = ('is_completed',)
     search_fields = ('title',)
     ordering = ('start_date',)
+    list_display_links = ('title',)
     fieldsets = (
-    ('todo Info', {
-        'fields': ('title', 'description', 'is_completed')
-    }),
-    ('Date Fange', {
-        'fields': ('start_date', 'end_date')
-    }),
+        ('Todo Info', {
+            'fields': ('user', 'title', 'description', 'is_completed')
+        }),
+        ('Date Range', {
+            'fields': ('start_date', 'end_date')
+        }),
+    )
+    inlines = [CommentInline]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'todo', 'user', 'message', 'created_at')
+    list_filter = ('todo', 'user')
+    search_fields = ('message', 'user')
+    ordering = ('-created_at',)
+    list_display_links = ('message',)
+    fieldsets = (
+        ('Comment Info', {
+            'fields': ('todo', 'user', 'message')
+        }),
     )
